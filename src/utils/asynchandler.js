4d35fd62a43f68cd09a21da2c196a87e156a5ca7
LalatenduR <143542838+LalatenduR@users.noreply.github.com>
2025-01-28 19:30:35 +0530

    all setup complete

--- src/utils/asynchandler.js
+++ src/utils/asynchandler.js
@@ -1,6 +1,8 @@
 
-const asynchandler = (fn) => (req, res, next) => {
+const asynchandler = (requesthandler) => {
+    return (req, res, next) => {
     Promise.resolve(requesthandler(req, res, next)).catch((err)=>next(err));
+    }
 };
 
 export {asynchandler}
